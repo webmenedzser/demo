92743db662ecf004c22336f3b8a4c307dcd38dc8
Ottó Radics <otto@webmenedzser.hu>
2019-11-28 22:04:03 +0100

    NEW: mailhog added, top comment & logging removed

--- config/app.php
+++ config/app.php
@@ -18,19 +18,48 @@ return [
         'my-module' => \modules\Module::class,
     ],
 
-    'components' => [
-        'redis' => [
-            'class' => yii\redis\Connection::class,
-            'hostname' => 'redis',
-            'port' => 6379
-        ],
-        'cache' => [
-            'class' => yii\redis\Cache::class,
-            'defaultDuration' => 86400,
-        ],
-        'session' => [
-            'class' => yii\redis\Session::class,
-            'as session' => craft\behaviors\SessionBehavior::class,
+    '*' => [
+        'components' => [
+            'redis' => [
+                'class' => yii\redis\Connection::class,
+                'hostname' => 'redis',
+                'port' => 6379
+            ],
+            'cache' => [
+                'class' => yii\redis\Cache::class,
+                'defaultDuration' => 86400,
+            ],
+            'session' => [
+                'class' => yii\redis\Session::class,
+                'as session' => craft\behaviors\SessionBehavior::class,
+            ]
+        ]
+    ],
+
+    'dev' => [
+        'components' => [
+            'mailer' => function() {
+                // Get the stored email settings
+                $settings = craft\helpers\App::mailSettings();
+
+                // Override the transport adapter class
+                $settings->transportType = craft\mail\transportadapters\Smtp::class;
+
+                // Override the transport adapter settings
+                $settings->transportSettings = [
+                    'host' => 'mailhog',
+                    'port' => '1025',
+                    'useAuthentication' => true,
+                    'username' => 'mailhog',
+                    'password' => 'mailhog'
+                ];
+
+                // Create a Mailer component config with these settings
+                $config = craft\helpers\App::mailerConfig($settings);
+
+                // Instantiate and return it
+                return Craft::createObject($config);
+            }
         ]
     ]
 ];
